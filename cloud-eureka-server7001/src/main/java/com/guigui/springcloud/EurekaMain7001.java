8fb3e21db6b4eb31bfb66279521c2c4b5c5b77ce
poipopopoi <254864339@qq.com>
2021-07-04 19:42:31 +0800

    6

--- cloud-eureka-server7001/src/main/java/com/guigui/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/guigui/springcloud/EurekaMain7001.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * @author Yang Hao
- * @description
- * @date 2020-09-14 21:47
- */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaMain7001 {
